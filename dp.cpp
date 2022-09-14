634d12a89ca7bd6b197bcb4e8a2a22b6cc5f8d37
Gaurav Chauhan <99039151+einsnicht@users.noreply.github.com>
2022-09-15 00:24:06 +0530

    commit

--- dp.cpp
+++ dp.cpp
@@ -67,6 +67,65 @@ void solve() {
 	}
 }
 
+
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    solve();
+  }
+  return 0;
+}
+
+// Ending form [LIS O(N * N)]
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int arr[10010];
+int dp[10010];
+
+int rec(int level, int left) {
+	// best LIS ending at that level 
+	// pruning 
+	if (level < 0) {
+		return 0;
+	}
+	// basecase
+	// cache check
+	if (dp[level] != -1) {
+		return dp[level];
+	}
+	// compute
+	int ans = 1;
+	for (int prev_taken = 0; prev_taken < level; prev_taken++) {
+		if (arr[prev_taken] < arr[level]) {
+			ans = max(ans, 1 + rec(prev_taken));
+		}
+	}
+	// save and return
+	return dp[level] = ans;
+}
+
+void solve() {
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+	memset(dp, -1, sizeof(dp));
+	int best = 0;
+	for (int i = 0; i < n; i++) {
+		best = max(best, rec(i));
+	}
+	cout << best << "\n";
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
