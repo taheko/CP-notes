6ddcea6a04319e99ffe55a2bc9ef51141ca77cc3
Gaurav Chauhan <99039151+einsnicht@users.noreply.github.com>
2022-09-19 16:50:31 +0530

    commit

--- dp.cpp
+++ dp.cpp
@@ -284,3 +284,54 @@ int main() {
   cout << dp.size() << "\n";
   return 0;
 }
+
+
+// Subsequence 0100 (can be solved using simple maths)
+// solution does not depend on the string 
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int dp[100010][5];
+
+string T = "0100";
+
+int rec(int level, int match) {
+  // pruning 
+  if (match == 4) {
+    return 0;
+  }
+  // base case
+  if (level == n) {
+    return 1;
+  }
+  // cache check
+  if (dp[level][match] != -1) {
+    return dp[level][match];
+  }
+  // compute 
+  int ans = rec(level + 1, match + 1) + rec(level + 1, match);
+
+  // save and return 
+  return dp[level][match] = ans;
+}
+
+void solve() {
+  cin >> n;
+  memset(dp, -1, sizeof(dp));
+  cout << rec(0, 0) << "\n";
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    solve();
+  }
+  return 0;
+}
